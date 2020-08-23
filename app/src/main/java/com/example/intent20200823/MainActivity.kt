2c481acdf95336ee197b019dde0d3a69f058c98a
tjcha <cha7713@gmail.com>
2020-08-23 16:23:17 +0900

    delete

--- app/src/main/java/com/example/intent20200823/MainActivity.kt
+++ app/src/main/java/com/example/intent20200823/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.intent20200823
 
+import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.text.set
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_move.setOnClickListener {
+            var msg = edit_text.text.toString()
+
+            if (msg==""){
+                val intent = Intent(this@MainActivity, OtherActivity::class.java)
+                this.startActivity(intent)
+            }else{
+                val intent = Intent(this@MainActivity, MessageActivity::class.java)
+                intent.putExtra("msg",msg)
+                this.startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/intent20200823/OtherActivity.kt
+++ app/src/main/java/com/example/intent20200823/OtherActivity.kt
@@ -1,11 +1,21 @@
 package com.example.intent20200823
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        btn_return.setOnClickListener {
+//            val intent = Intent(this@OtherActivity, MainActivity::class.java)
+//            this.startActivity(intent)
+//            this.finish()
+            onBackPressed()
+        }
     }
+
 }
